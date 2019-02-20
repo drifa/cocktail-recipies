83a1dac27f9d8b6d12de1cd6944d4b177156b354
Drífa Sóley Heimisdóttir <drifasoley@Drifas-MacBook-Pro-2.local>
2019-02-20 16:19:57 +0000

    recipe site setup

--- src/App.js
+++ src/App.js
@@ -1,47 +1,59 @@
 import React, { Component } from 'react';
 import Recipe from './components/Recipe';
+import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
 
-import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import theme from './Theme';
+
+import './App.scss';
 
 class App extends Component {
   state = {
+    value: 0,
     recipe: [
       {
         id: 1,
         title: 'Moscow Mule',
+        about: 'Something about the drink',
+        img: 'images/teaDrink.jpg',
         ingredients: [
           {
+            id: 1,
             ingredient: 'Vodka',
             quantity: '2 oz.',
           },
           {
+            id: 2,
             ingredient: 'Lime juice',
             quantity: '1/2 oz.',
           },
           {
+            id: 3,
             ingredient: 'Ginger beer',
             quantity: '5 oz.',
           },
           {
+            id: 4,
             ingredient: 'Lime wedge for garnish',
             quantity: '1 wedge',
           }
         ],
         instruction: [
           {
-            number: 1,
+            number: '1.',
             text: 'Pour vodka and squeeze lime juice into your glass/mug',
           },
           {
-            number: 2,
+            number: '2.',
             text: 'Add ice cubes and then fill your glass/mug with ginger beer',
           },
           {
-            number: 3,
+            number: '3.',
             text: 'Garnish with a lime wedge, and enjoy!',
           },
         ],
-        equipment: [
+        equipments: [
           {
             type: 'Glass or mug',
             quantity: 1
@@ -75,9 +87,24 @@ class App extends Component {
     console.log(this.state.recipe);
     return (
       <div className="App">
-        <header className="App-header">
-          <Recipe recipe = {this.state.recipe} />
-        </header>
+        <MuiThemeProvider theme={theme}>
+          <AppBar position='static'>
+            <div>
+              <Button>
+                <div>Button 1</div>
+              </Button>
+              <Button>
+                <div>Button 2</div>
+              </Button>
+            </div>
+          </AppBar>
+        </MuiThemeProvider>
+
+        <div className='parallax'>
+          <div className='parallax-container'>
+            <Recipe recipe = {this.state.recipe} />
+          </div>
+        </div>
       </div>
     );
   }
--- src/components/RecipeDescription.js
+++ src/components/RecipeDescription.js
@@ -1,56 +1,115 @@
 import React, { Component } from 'react'
 import propTypes from 'prop-types';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import { Card, CardActions, CardContent, CardHeader, Typography } from '@material-ui/core';
+
+import '../styles/RecipeDescription.scss';
+
+class CardIngredients extends Component {
+    render() {
+        return (
+            <Card className='ingredients-container'>
+                <CardHeader title='Ingredients'/>
+
+                {this.props.ingredients.map((ingredientInfo) => 
+                <CardActions className='ingredients-cocktail' key={ingredientInfo.id}>
+                    <CardActions className='ingredient-info'>
+                        <Checkbox className='ingredients-info' value="checkedC" />
+
+                        <Typography className='ingredients-info'>
+                            {ingredientInfo.quantity}
+                        </Typography>
+
+                        <Typography className='ingredients-info'>
+                            {ingredientInfo.ingredient}
+                        </Typography>
+                    </CardActions>
+                    
+                </CardActions>
+                )}
+            </Card>
+        );
+    }
+}
+
+class CardInstructions extends Component {
+    render() {
+        return (
+            <Card className='instructions-container'>
+                <CardHeader title='Instructions' />
+
+                {this.props.instruction.map((instructionInfo) => 
+                <CardContent className='instruction-cocktail' key={instructionInfo.id}>
+
+                    <CardActions className='instruction-number'>
+                        <Typography>
+                            {instructionInfo.number}
+                        </Typography>
+                    <CardActions>
+                    
+                    </CardActions>
+                        <Typography>
+                            {instructionInfo.text}
+                        </Typography>
+                    </CardActions>
+
+                </CardContent>
+                )}
+            </Card>
+        );
+    }
+}
+
+class CardEquipment extends Component {
+    render() {
+        return (
+            <Card className='equipment-container'>
+                <CardHeader title='Equipment' />
+
+                {this.props.equipments.map((equipment) => 
+                <CardActions className='equipment-cocktail' key={equipment.id}>
+
+                    <CardActions className='equipment-info'>
+                        <Checkbox value="checkedC" />
+
+                        <Typography>
+                            {equipment.quantity}
+                        </Typography>
+
+                        <Typography>
+                            {equipment.type}
+                        </Typography>
+                    </CardActions>
+
+                </CardActions>
+                )}
+            </Card>
+        );
+    }
+}
 
 export class RecipeDescription extends Component {
   render() {
-      const { ingredients, instruction, equipment} = this.props.recipeInfo;
     return (
-        <div>
-            {ingredients.map((ingredientInfo) => 
-            <div key={ingredientInfo.id}>
-                <div>
-                    <p>
-                        {ingredientInfo.ingredient}
-                    </p>
-                </div>
-                <div>
-                    <p>
-                        {ingredientInfo.quantity}
-                    </p>
-                </div>
-            </div>
-            )}
+        <Grid container spacing={24} id='cocktail-info-grid-container'>
 
-            {instruction.map((instructionInfo) => 
-            <div key={instructionInfo.id}>
-                <div>
-                    <p>
-                        {instructionInfo.number}
-                    </p>
-                </div>
-                <div>
-                    <p>
-                        {instructionInfo.text}
-                    </p>
-                </div>
-            </div>
-            )}
+            <Grid container item spacing={24} xs id='ingredients-equipment-grid'>
+                <Grid item id='ingredients-grid' xs={12}>
+                    <CardIngredients ingredients={this.props.recipeInfo.ingredients}/>
+                </Grid>
+                <Grid item xs={12}>
+                    <CardEquipment equipments={this.props.recipeInfo.equipments}/>
+                </Grid>
+            </Grid>
 
-            {equipment.map((equipments) => 
-            <div key={equipments.id}>
-                <div>
-                    <p>
-                        {equipments.type}
-                    </p>
-                </div>
-                <div>
-                    <p>
-                        {equipments.quantity}
-                    </p>
-                </div>
-            </div>
-            )}
-        </div>
+            <Grid container item spacing={24} xs id='instruction-grid'>
+                <Grid item xs={12}>
+                    <CardInstructions instruction={this.props.recipeInfo.instruction}/>
+                </Grid>
+            </Grid>
+
+        </Grid>
     );
   }
 }
@@ -58,6 +117,6 @@ export class RecipeDescription extends Component {
 // PropTypes
 RecipeDescription.propTypes = {
     recipeInfo: propTypes.object.isRequired
-  }
+}
 
 export default RecipeDescription
